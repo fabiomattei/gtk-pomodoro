790a72bdb111a70e9a971171a97be2412f19ad74
Fabio Mattei <burattino@gmail.com>
2019-08-08 22:47:58 +0200

    working on the model

--- application/ui/pomodoro/application.rb
+++ application/ui/pomodoro/application.rb
@@ -1,9 +1,16 @@
 module Pomodoro
 
   class Application < Gtk::Application
+    attr_reader :user_data_path
 
     def initialize
       super 'net.funambolo.gtk-pomodoro', Gio::ApplicationFlags::FLAGS_NONE
+
+      @user_data_path = File.expand_path('~/.gtk-pomodoro')
+      unless File.directory?(@user_data_path)
+        puts "First run. Creating user's application path: #{@user_data_path}"
+        FileUtils.mkdir_p(@user_data_path)
+      end
 
       signal_connect :activate do |application|
         window = Gtk::ApplicationWindow.new(application)
