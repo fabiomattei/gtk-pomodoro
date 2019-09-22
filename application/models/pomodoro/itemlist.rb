75cd4a27f1aa8c3e80a0971cb6e6319378b0961f
Fabio Mattei <burattino@gmail.com>
2019-09-22 09:46:56 +0200

    working on file management

--- application/models/pomodoro/itemlist.rb
+++ application/models/pomodoro/itemlist.rb
@@ -1,7 +1,27 @@
 module Pomodoro
   
   class ItemList
-  	
+
+  	def initialize( filename )
+  		@filename = filename
+  	end
+
+	# It load items from its `filename` location
+  	def load_items_list
+
+  		file = File.open @filename
+		data = JSON.load file
+		file.close
+
+    end
+
+    # It saves items to its `filename` location
+    def save!
+      File.open(@filename, 'w') do |file|
+        file.write self.to_json
+      end
+    end
+
   end
 
 end
